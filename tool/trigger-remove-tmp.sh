9db9e8cebf038ef9ed24b37cff2509cea2c736d9
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2021-12-27 17:53:23 +0900

    Use gh's token if hub's token does not exist

--- tool/trigger-remove-tmp.sh
+++ tool/trigger-remove-tmp.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-: ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", 0, "oauth_token")' ~/.config/hub)}
+if [ -f ~/.config/hub ]; then
+  : ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", 0, "oauth_token")' ~/.config/hub)}
+fi
+if [ -f ~/.config/gh/hosts.yml ]; then
+  : ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", "oauth_token")' ~/.config/gh/hosts.yml)}
+fi
+
 : ${1?Usage: $0 2.4.9-draft}
 curl -sS -X POST -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' -H "Authorization: token $TOKEN" https://api.github.com/repos/ruby/actions/dispatches --data '{"event_type": "Remove pub/tmp/ruby-'"${1}"'"}'
--- tool/trigger-snapshot.sh
+++ tool/trigger-snapshot.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-: ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", 0, "oauth_token")' ~/.config/hub)}
-curl -v -X POST -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' -H "Authorization: token $TOKEN" https://api.github.com/repos/ruby/actions/dispatches --data '{"event_type": "'"${1:-make-snapshot}"'"}'
+if [ -f ~/.config/hub ]; then
+  : ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", 0, "oauth_token")' ~/.config/hub)}
+fi
+if [ -f ~/.config/gh/hosts.yml ]; then
+  : ${TOKEN:=$(ruby -r yaml -e 'puts YAML.load(ARGF.read).dig("github.com", "oauth_token")' ~/.config/gh/hosts.yml)}
+fi
+
+curl -v -X POST -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' -H "Authorization: token ${TOKEN:?}" https://api.github.com/repos/ruby/actions/dispatches --data '{"event_type": "'"${1:-make-snapshot}"'"}'
