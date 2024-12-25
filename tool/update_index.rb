61579770d3921164576cf769919076225082fe27
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2024-12-25 18:04:02 +0900

    Add 3.4 to DIRS

--- tool/update_index.rb
+++ tool/update_index.rb
@@ -19,7 +19,7 @@ EXT_NAMES = %w(.gz .bz2 .zip .xz).freeze
 DIRS = %w(
   1.0 1.1a 1.1b 1.1c 1.1d 1.2 1.3 1.4 1.6 1.8 1.9
   2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7
-  3.0 3.1 3.2 3.3
+  3.0 3.1 3.2 3.3 3.4
 ).freeze
 
 def create_index(bucket)
